554b6e862aa4d60a12b9b07ca8c814cdc43883fd
naitsircnat <cristiangerardchen@gmail.com>
2025-01-21 05:30:33 +0000

    Added data layer

---
+++ data/productData.js
@@ -0,0 +1,22 @@
+const pool = require("../database");
+
+async function getAllProducts() {
+  const [rows] = await pool.query(
+    "SELECT name, CAST(price AS DOUBLE) AS price, image * FROM products"
+  );
+  return rows;
+}
+
+async function getProductById(id) {
+  const [rows] = await pool.query(
+    "SELECT name, CAST(price AS DOUBLE) AS price, image * FROM products WHERE id = ?",
+    [id]
+  );
+
+  return rows[0];
+}
+
+module.exports = {
+  getAllProducts,
+  getProductById,
+};
