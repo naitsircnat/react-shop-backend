ff0c5ea7597e5a25401737f23d4272ecc8006054
naitsircnat <cristiangerardchen@gmail.com>
2025-01-22 08:47:11 +0000

    Completed user router portion (pending testing)

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 require("dotenv").config();
 const pool = require("./database.js");
 const productsRouter = require("./routes/products");
+const usersRouter = require("./routes/users");
 
 const app = express();
 
@@ -10,6 +11,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/api/products", productsRouter);
+app.use("/api/users", usersRouter);
 
 app.get("/", (req, res) => {
   res.json({ message: "Hello world!" });
--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,83 @@
 const express = require("express");
 const router = express.Router();
+const userServices = require("../services/userServices");
+const jwt = require("jsonwebtoken");
+const AuthenticateWithJwt = require("../middlewares/AuthenticateWithJwt");
 
-router.post("/", (req, res) => {
-  res.json({ Message: "Register a new user" });
+router.post("/register", async (req, res) => {
+  try {
+    const { name, email, password, country, salutation, marketingPreferences } =
+      req.body;
+
+    const userId = await userServices.registerUser({
+      name,
+      email,
+      password,
+      country,
+      salutation,
+      marketingPreferences,
+    });
+
+    res.status(201).json({ message: "Registration successful", userId });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 });
 
-router.post("/login", (req, res) => {
-  res.json({ Message: "Log in a user" });
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await userServices.loginUser(email, password);
+
+    const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+
+    res.json({ message: "Login successful", token, user_id: user.id });
+  } catch (error) {
+    res.status(401).json({ message: error.message });
+  }
+});
+
+router.put("/me", AuthenticateWithJwt, async (req, res) => {
+  try {
+    const userId = req.userId;
+    const userDetails = req.body;
+
+    await userServices.updateUserDetails(userId, userDetails);
+
+    res.json({ message: "User details updated successfully." });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+router.delete("/me", AuthenticateWithJwt, async (req, res) => {
+  try {
+    const userId = req.userId;
+
+    await userServices.deleteUserAccount(userId);
+    res.json({ message: "User deleted successfully" });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+router.get("/me", AuthenticateWithJwt, async (req, res) => {
+  try {
+    const userId = req.userId;
+
+    const user = await userServices.getUserDetailsById(userId);
+
+    if (!user) {
+      res.status(400).json({ message: "User not found" });
+    }
+
+    res.json({ user });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
 module.exports = router;
