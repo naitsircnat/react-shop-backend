dc2dd7f2d446143704f430013864d4f792bb06a3
naitsircnat <cristiangerardchen@gmail.com>
2025-01-24 05:06:07 +0000

    Added service layer for cart

---
+++ services/cartServices.js
@@ -0,0 +1,15 @@
+const cartData = require("../data/cartData");
+
+async function getCartContents(userId) {
+  await cartData.getCart(userId);
+}
+
+async function updateCartContents(userId, newCartContents) {
+  if (!Array.isArray(newCartContents)) {
+    throw new Error("New cart needs to be an array.");
+  }
+
+  await cartData.updateCart(userId, newCartContents);
+}
+
+module.exports = { getCartContents, updateCartContents };
