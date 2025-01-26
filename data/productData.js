5ccd03536d8b7cbc1a24aa11c01097c014a18ada
naitsircnat <cristiangerardchen@gmail.com>
2025-01-26 12:36:48 +0000

    Adjusted getAllProducts data later

--- data/productData.js
+++ data/productData.js
@@ -2,7 +2,7 @@ const pool = require("../database");
 
 async function getAllProducts() {
   const [rows] = await pool.query(
-    "SELECT name, CAST(price AS DOUBLE) AS price, image FROM products"
+    "SELECT id, name, CAST(price AS DOUBLE) AS price, image FROM products"
   );
   return rows;
 }
