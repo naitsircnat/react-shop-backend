84723f1974847590922e251a7e9d24daa6757ecd
naitsircnat <cristiangerardchen@gmail.com>
2025-02-13 04:37:51 +0000

    Created checkoutservices.js

---
+++ services/checkoutServices.js
@@ -0,0 +1,30 @@
+/*
+-imports
+-checkout - need userId, Items, orderId
+--return session
+-export 
+*/
+
+const cartService = require("./cartServices");
+const orderService = require("./orderServices");
+const stripeService = require("./stripeServices");
+
+async function checkout(userId) {
+  const orderItems = await cartService.getCartContents(userId);
+
+  const orderId = await orderService.createOrder(userId, orderItems);
+
+  const session = await stripeService.createCheckoutSession(
+    userId,
+    orderItems,
+    orderId
+  );
+
+  await orderService.updateOrderSessionId(orderId, session.id);
+
+  return session;
+}
+
+module.exports = {
+  checkout,
+};
