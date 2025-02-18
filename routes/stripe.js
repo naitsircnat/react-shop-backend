c389758e2cb7d97a64b0ced71ac365b0f1ac166d
naitsircnat <cristiangerardchen@gmail.com>
2025-02-18 02:58:31 +0000

    Added stripe.js file

---
+++ routes/stripe.js
@@ -0,0 +1,38 @@
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+const orderServices = require("../services/orderServices");
+
+router.post(
+  "/webhook",
+  express.raw({ type: "application/json" }),
+  async (req, res) => {
+    let event;
+    try {
+      let sig = req.headers["stripe-signature"];
+      event = stripe.webhooks.constructEvent(
+        req.body,
+        sig,
+        process.env.STRIPE_WEBHOOK_SECRET
+      );
+    } catch (error) {
+      console.error("Webhook error: ", error.message);
+      return res.status(400).send(`Webhook error: ${error.message}`);
+    }
+
+    switch (event.type) {
+      case "checkout.session.completed":
+        const session = event.data.object;
+        console.log("Checkout session completed!", session);
+        if (session.metadata && session.metadata.orderId) {
+          await orderServices.updateOrderStatus(
+            session.metadata.orderId,
+            "Processing"
+          );
+        }
+        break;
+      default:
+        console.log(`Unhandled event type ${event.type}`);
+    }
+
+    res.json({ received: true });
+  }
+);
