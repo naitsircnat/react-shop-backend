f574a28ed3d8903d76d906f74674015bb53e203a
naitsircnat <cristiangerardchen@gmail.com>
2025-02-26 07:41:53 +0000

    Fixed error in data.sql

--- data.sql
+++ data.sql
@@ -4,7 +4,7 @@ INSERT INTO products (name, price, description, image) VALUES
 ('Ethiopian Sunrise', 15.99, "A bright and fruity coffee with floral notes.", 'https://picsum.photos/id/20/300/200'),
 ('Colombian Gold', 12.99, "Smooth and balanced with a caramel finish.",'https://picsum.photos/id/1/300/200'),
 ('Sumatran Velvet', 17.99, "Bold and earthy with hints of dark chocolate",'https://picsum.photos/id/26/300/200'),
-('Brazilian Bliss', 14.99, 'https://picsum.photos/id/96/300/200');
+('Brazilian Bliss', 14.99, "Rich and nutty with a creamy body.", 'https://picsum.photos/id/96/300/200');
 
 INSERT INTO marketing_preferences (id, preference) VALUES (1, 'email');  -- Email Marketing
 INSERT INTO marketing_preferences (id, preference) VALUES (2, 'sms');    -- SMS Marketing
