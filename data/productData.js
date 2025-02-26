fc90b150ca11d3ed20539c647ec55d4382d99b9b
naitsircnat <cristiangerardchen@gmail.com>
2025-02-26 08:13:06 +0000

    Updated getAllProducts function in data.js

--- data/productData.js
+++ data/productData.js
@@ -1,9 +1,7 @@
 const pool = require("../database");
 
 async function getAllProducts() {
-  const [rows] = await pool.query(
-    "SELECT id, name, CAST(price AS DOUBLE) AS price, image FROM products"
-  );
+  const [rows] = await pool.query("SELECT * FROM products");
   return rows;
 }
 
