77472a04967b11198854c253d6e9f534cd3e171e
naitsircnat <cristiangerardchen@gmail.com>
2025-02-26 07:28:03 +0000

    Updated schema and data.sql with coffee products

--- data.sql
+++ data.sql
@@ -1,11 +1,13 @@
 use ecommerce;
 
-INSERT INTO products (name, price, image) VALUES
-('Sleek Smartwatch', 199.99, 'https://picsum.photos/id/20/300/200'),
-('Wireless Earbuds', 79.99, 'https://picsum.photos/id/1/300/200'),
-('Portable Power Bank', 49.99, 'https://picsum.photos/id/26/300/200'),
-('HD Action Camera', 129.99, 'https://picsum.photos/id/96/300/200');
+INSERT INTO products (name, price, description, image) VALUES
+('Ethiopian Sunrise', 15.99, "A bright and fruity coffee with floral notes.", 'https://picsum.photos/id/20/300/200'),
+('Colombian Gold', 12.99, "Smooth and balanced with a caramel finish.",'https://picsum.photos/id/1/300/200'),
+('Sumatran Velvet', 17.99, "Bold and earthy with hints of dark chocolate",'https://picsum.photos/id/26/300/200'),
+('Brazilian Bliss', 14.99, 'https://picsum.photos/id/96/300/200');
 
 INSERT INTO marketing_preferences (id, preference) VALUES (1, 'email');  -- Email Marketing
 INSERT INTO marketing_preferences (id, preference) VALUES (2, 'sms');    -- SMS Marketing
+
+
 
--- schema.sql
+++ schema.sql
@@ -7,6 +7,7 @@ CREATE TABLE products (
   id INT AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(255) NOT NULL,
   price DECIMAL(10, 2) NOT NULL,
+  description VARCHAR(255) NOT NULL,
   image VARCHAR(255) NOT NULL
 );
 
