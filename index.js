b21e00170408231a9dfa881728e3caddc548e3d1
naitsircnat <cristiangerardchen@gmail.com>
2025-05-07 05:59:54 +0000

    Adjusted .env and port details

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.get("/", (req, res) => {
 
 // const PORT = process.env.port || 3000;
 // const PORT = process.env.PORT || 3000;
-const PORT = process.env.PORT;
+const PORT = process.env.port;
 
 app.listen(PORT, () => {
   console.log(`Server started on ${PORT}`);
