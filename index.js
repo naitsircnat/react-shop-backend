fab326ea651f47dfab0a59e5869417d6742c1fd3
naitsircnat <cristiangerardchen@gmail.com>
2025-05-07 06:03:01 +0000

    adjusted port details

--- index.js
+++ index.js
@@ -28,8 +28,7 @@ app.get("/", (req, res) => {
 });
 
 // const PORT = process.env.port || 3000;
-// const PORT = process.env.PORT || 3000;
-const PORT = process.env.port;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server started on ${PORT}`);
