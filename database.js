736cd921c48f41cf7b243517a8b037be6cd71b04
naitsircnat <cristiangerardchen@gmail.com>
2025-05-09 07:38:24 +0000

    Updated database.js file to add port details

--- database.js
+++ database.js
@@ -1,10 +1,21 @@
 const mysql2 = require("mysql2/promise");
 
+// const pool = mysql2.createPool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   database: process.env.DB_NAME,
+//   password: process.env.DB_PASSWORD,
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0,
+// });
+
 const pool = mysql2.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
