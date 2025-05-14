4918f4f8ca6eba21c767209ebe1f7098939f4f91
naitsircnat <cristiangerardchen@gmail.com>
2025-05-14 07:07:43 +0000

    Reverted database.js

--- database.js
+++ database.js
@@ -24,8 +24,7 @@ const mysql2 = require("mysql2/promise");
 // FOR DEPLOYMENT USING RAILWAY
 
 const pool = mysql2.createPool({
-  // host: process.env.MYSQLHOST,
-  host: "mysql.railway.internal",
+  host: process.env.MYSQLHOST,
   user: process.env.MYSQLUSER,
   database: process.env.MYSQLDATABASE,
   password: process.env.MYSQLPASSWORD,
